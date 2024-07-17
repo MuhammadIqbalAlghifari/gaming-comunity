361ffaad95e46946dc47bf4e3b00d6debddbb042
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2024-07-17 15:24:40 +0700

    added smooth scrolling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Navbar from './components/Navbar.vue';
+import Lenis from 'lenis'
+import { onMounted } from 'vue';
+onMounted(() => {
+  const lenis = new Lenis()
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  requestAnimationFrame(raf)
+});
 </script>
 
 <template>
