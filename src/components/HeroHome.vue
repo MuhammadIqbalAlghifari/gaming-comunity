97a87efc34000429d2fe973bdbc941bf3ba36d1a
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2024-11-29 15:22:39 +0700

    final

--- src/components/HeroHome.vue
+++ src/components/HeroHome.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="w-full lg:h-screen h-full max-w-screen-2xl mx-auto flex flex-wrap justify-between lg:px-20 lg:py-0 md:pt-24 md:px-14 pt-20 px-8 items-center">
+    <main class="w-full lg:h-screen h-full xl:max-w-[1280px] xl:px-0 mx-auto flex flex-wrap justify-between lg:px-20 lg:py-0 md:pt-24 md:px-14 pt-20 px-8 items-center">
         <div ref="leftHero" class="flex lg:w-1/2 w-full flex-col justify-center items-center gap-y-4 lg:items-start">
             <p class="text-orange-600 lg:text-base md:text-sm text-xs font-bold">3D game dev</p>
             <h1 class="text-white lg:text-5xl md:text-3xl text-xl max-w-sm leading-normal lg:leading-normal text-center lg:text-start font-bold">Work that we produce for our clients</h1>
@@ -7,7 +7,7 @@
             <button class="bg-orange-500 lg:px-8 lg:py-4 px-6 py-2 lg:text-sm text-xs rounded-full text-white">Get more details</button>
         </div>
         <div ref="rightHero" class="lg:w-1/2 lg:py-0 py-5 w-full h-full relative flex justify-center">
-            <img src="/joy-stick.png" alt="" class="w-[80%] -z-10 h-full">
+            <img src="/joy-stick.png" alt="" class="w-[80%] -z-10 h-full xl:h-[80%]">
             <div class="w-full flex flex-col justify-end gap-y-10 items-center md:h-[50%] h-[55%] absolute z-10">
                 <div class="w-[75%] flex justify-between items-center">
                     <img src="/cry-engine.png" alt="" class="md:w-auto md:h-auto w-auto h-auto object-cover md:object-none">
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,23 @@
 <template>
-    <header ref="navbarRef" class="fixed z-50 w-full">
-      <nav class="flex md:px-14 md:py-7 p-5 justify-between items-center w-full bg-[#050301] text-white">
-        <MobileNav :open="open" @set-open="setOpen" />
-        <h1 class="lg:text-2xl md:text-xl text-lg font-bold cursor-pointer">LOGO</h1>
-        <ul class="md:flex hidden items-center justify-between w-[45%] xl:text-sm text-xs">
-          <RouterLink class="hover:text-orange-400 transition-all duration-300" to="/">Home</RouterLink>
-          <RouterLink class="hover:text-orange-400 transition-all duration-300" to="/about">About Us</RouterLink>
-          <RouterLink class="hover:text-orange-400 transition-all duration-300" to="/portfolio">Portfolio</RouterLink>
-          <RouterLink class="hover:text-orange-400 transition-all duration-300" to="/news">News</RouterLink>
-          <RouterLink to="/contact" class="hover:text-orange-400 transition-all duration-300">Contact Us</RouterLink>
-        </ul>
-        <div class="flex relative w-8 h-8 flex-col justify-between items-center md:hidden" @click="toggleOpen">
-          <span :class="['h-1 w-full rounded-lg transform transition duration-300 ease-in-out', open ? 'rotate-45 translate-y-3.5 bg-white' : 'bg-white']"></span>
-          <span :class="['h-1 w-full rounded-lg transform transition-all duration-300 ease-in-out', open ? 'w-0 h-0' : 'bg-white w-full']"></span>
-          <span :class="['h-1 w-full rounded-lg transform transition duration-300 ease-in-out', open ? '-rotate-45 -translate-y-3.5 bg-white' : 'bg-white']"></span>
-        </div>
-      </nav>
+    <header ref="navbarRef" class="fixed z-50 w-full flex justify-center items-center bg-[#050301]">
+      <div class="xl:max-w-[1280px] w-full">
+        <nav class="flex xl:px-0 md:px-14 md:py-7 p-5 justify-between items-center w-full text-white">
+          <MobileNav :open="open" @set-open="setOpen" />
+          <h1 class="lg:text-2xl md:text-xl text-lg font-bold cursor-pointer">LOGO</h1>
+          <ul class="md:flex hidden items-center justify-between w-[45%] xl:text-sm text-xs">
+            <RouterLink class="hover:text-orange-400 transition-all duration-300" to="/">Home</RouterLink>
+            <RouterLink class="hover:text-orange-400 transition-all duration-300" to="/about">About Us</RouterLink>
+            <RouterLink class="hover:text-orange-400 transition-all duration-300" to="/portfolio">Portfolio</RouterLink>
+            <RouterLink class="hover:text-orange-400 transition-all duration-300" to="/news">News</RouterLink>
+            <RouterLink to="/contact" class="hover:text-orange-400 transition-all duration-300">Contact Us</RouterLink>
+          </ul>
+          <div class="flex relative w-8 h-8 flex-col justify-between items-center md:hidden" @click="toggleOpen">
+            <span :class="['h-1 w-full rounded-lg transform transition duration-300 ease-in-out', open ? 'rotate-45 translate-y-3.5 bg-white' : 'bg-white']"></span>
+            <span :class="['h-1 w-full rounded-lg transform transition-all duration-300 ease-in-out', open ? 'w-0 h-0' : 'bg-white w-full']"></span>
+            <span :class="['h-1 w-full rounded-lg transform transition duration-300 ease-in-out', open ? '-rotate-45 -translate-y-3.5 bg-white' : 'bg-white']"></span>
+          </div>
+        </nav>
+      </div>
     </header>
   </template>
   
